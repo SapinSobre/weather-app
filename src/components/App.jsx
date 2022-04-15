d764bd025892e67fb583fd2eac06d507c088bd53
Nathalie Daoût <nat.dao3@gmail.com>
2022-04-15 10:32:02 +0200

    sass

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ function App() {
     <div className="main">
       <Banner/>
       <Main/>
-      <Footer/>   
     </div>
   );
 }
--- src/components/DisplayResults.jsx
+++ src/components/DisplayResults.jsx
@@ -2,14 +2,14 @@ import '../sass/DisplayResults.scss'
 
 const DisplayResults = ({photo, setPhoto, cityvalue, setCityvalue}) => {
     return(
-        <div style={{backgroundImage: `url(${photo})`}} className="cityValuesBox">
-            <div className="cityValuesBox_input">{cityvalue.location}</div>
-            <div className="cityValuesBox_input">{cityvalue.temp}</div>
-            <div className="cityValuesBox_input">{cityvalue.weather}</div>
-            <div className="cityValuesBox_input">{cityvalue.feels}</div>
-            <div className="cityValuesBox_input">{cityvalue.humidity}</div>
-            <div className="cityValuesBox_input">{cityvalue.wind}</div>
-        </div>        
+        <ul style={{backgroundImage: `url(${photo})`}} className="cityValuesBox">
+            <li className="cityValuesBox_val">{cityvalue.location}</li>
+            <li className="cityValuesBox_val">{cityvalue.temp}</li>
+            <li className="cityValuesBox_val">{cityvalue.weather}</li>
+            <li className="cityValuesBox_val">{cityvalue.feels}</li>
+            <li className="cityValuesBox_val">{cityvalue.humidity}</li>
+            <li className="cityValuesBox_val">{cityvalue.wind}</li>
+        </ul>        
     )
 }
 export default DisplayResults    
