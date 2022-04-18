f5d1ac7723ec2856635cb2f52b9abb4f991fb469
Nathalie Daoût <nat.dao3@gmail.com>
2022-04-18 02:20:34 +0200

    forecast

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,20 @@
 import Form from './Form'
 import DisplayResults from './DisplayResults'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import '../sass/Main.scss'
 
 const Main = () => {
     const [cityForecastValue, setCityForecastValue] = useState([])
     const [photo, setPhoto] = useState()
-    const [cityvalue, setCityvalue] = useState([])
+
+    const LSKEY = "weatherApp"
+    const savedCity = localStorage.getItem({LSKEY} + '.city')
+    const [cityvalue, setCityvalue] = useState(savedCity? JSON.parse(savedCity):[])
+    useEffect(() => {
+          window.localStorage.setItem({LSKEY} + '.city', JSON.stringify(cityvalue))
+      },[cityvalue]) 
+      
+
     return(
         <div className="main">
             <Form cityForecastValue={cityForecastValue} setCityForecastValue={setCityForecastValue} photo={photo} setPhoto={setPhoto} cityvalue={cityvalue} setCityvalue={setCityvalue}/>   
